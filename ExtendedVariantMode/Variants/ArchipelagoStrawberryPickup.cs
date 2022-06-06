7be0c007379d6718ffa7213927317ca42fff770e
Chad Hansen <chadmhansen@hotmail.com>
2022-06-06 09:38:55 -0700

    Working AP connection and ability to send two locations

--- ExtendedVariantMode/Variants/ArchipelagoStrawberryPickup.cs
+++ ExtendedVariantMode/Variants/ArchipelagoStrawberryPickup.cs
@@ -1,19 +1,26 @@
-﻿using Celeste;
+﻿using Archipelago.MultiClient.Net;
+using Archipelago.MultiClient.Net.Enums;
+using Archipelago.MultiClient.Net.Helpers;
+using Celeste;
 using Celeste.Mod;
 using MonoMod.RuntimeDetour;
 using MonoMod.Utils;
+using Monocle;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Archipelago.MultiClient.Net.Packets;
 
 namespace ExtendedVariants.Variants
 {
     class ArchipelagoStrawberryPickup : AbstractExtendedVariant
     {
         private ILHook collectRoutineHook;
+        private ArchipelagoSession currentAPSession;
+        private Dictionary<long, string> itemLookupCache = new Dictionary<long, string>();
 
         public override object GetDefaultVariantValue()
         {
@@ -32,38 +39,38 @@ namespace ExtendedVariants.Variants
 
         public override void Load()
         {
-            On.Celeste.Strawberry.OnPlayer += onStrawberryCollected;
+            On.Celeste.Strawberry.CollectRoutine += Strawberry_CollectRoutine;
+            On.Celeste.Session.ctor += onSessionStart;
 
             //collectRoutineHook = new ILHook(typeof(Strawberry).GetMethod("CollectRoutine", BindingFlags.NonPublic | BindingFlags.Instance).GetStateMachineTarget(), patchAllGoldenFlags);
         }
 
-        private void onStrawberryCollected(On.Celeste.Strawberry.orig_OnPlayer orig, Strawberry self, Player player)
+        private System.Collections.IEnumerator Strawberry_CollectRoutine(On.Celeste.Strawberry.orig_CollectRoutine orig, Strawberry self, int collectIndex)
         {
-            // if the strawberry is actually collected by the player, vanilla code switches ReturnHomeWhenLost to true.
-            // so, we'll use that to spare us some reflection or IL modding.
-            bool origReturnHome = self.ReturnHomeWhenLost;
-            self.ReturnHomeWhenLost = false;
+            Logger.Log("strawberry", self.ID.ToString());
+            Logger.Log("collectIndex", collectIndex.ToString());
 
-            orig(self, player);
+            Session session = self.SceneAs<Level>().Session;
+            Logger.Log("Level", session.Area.GetSID());
+            Logger.Log("Chapter", session.Area.ChapterIndex.ToString());
+            Logger.Log("Count Safe", SaveData.Instance.TotalStrawberries_Safe.ToString());
+            Logger.Log("Count", SaveData.Instance.TotalStrawberries.ToString());
 
-            bool wasCollected = self.ReturnHomeWhenLost;
-            self.ReturnHomeWhenLost = origReturnHome;
+            // Celeste/1-ForsakenCity
+            string locationName = session.Area.GetSID().Split('-')[1] + ' ' + self.ID.Key.Split(':')[0];
+            Logger.Log("Location Complete", locationName);
 
-            if (wasCollected)
+            long apID = this.currentAPSession.Locations.GetLocationIdFromName("Celeste", locationName);
+            //long apID = locationIds[self.ID.Key];
+
+            Logger.Log("AP ID", apID.ToString());
+            if (apID != -1)
             {
-                // store the inventory to restore it if needed.
-                new DynData<Strawberry>(self)["playerInventoryOnCollection"] = player.Inventory;
-
-                // Add AP logic here
-                Logger.Log("strawberry", self.ID.ToString());
-
-                Session session = self.SceneAs<Level>().Session;
-                Logger.Log("Level", session.Area.GetSID());
-                Logger.Log("Chapeter", session.Area.ChapterIndex.ToString());
-                Logger.Log("Count Safe", SaveData.Instance.TotalStrawberries_Safe.ToString());
-                Logger.Log("Count", SaveData.Instance.TotalStrawberries.ToString());
-
+                this.currentAPSession.Locations.CompleteLocationChecks(apID);
             }
+
+            return orig(self, collectIndex);
+
         }
 
         public override void SetLegacyVariantValue(int value)
@@ -73,12 +80,79 @@ namespace ExtendedVariants.Variants
 
         public override void Unload()
         {
-            On.Celeste.Strawberry.OnPlayer -= onStrawberryCollected;
+            On.Celeste.Strawberry.CollectRoutine -= Strawberry_CollectRoutine;
+            On.Celeste.Session.ctor -= onSessionStart;
         }
 
         protected override void DoSetVariantValue(object value)
         {
             Settings.Archipelago = (bool) value;
         }
+
+        public void onSessionStart(On.Celeste.Session.orig_ctor orig, Session self)
+        {
+            orig(self);
+
+            if (currentAPSession == null)
+            {
+                currentAPSession = ArchipelagoSessionFactory.CreateSession("localhost", 38281);
+
+                currentAPSession.TryConnectAndLogin("Celeste", "Chadbert", new Version(0, 3, 2), ItemsHandlingFlags.AllItems);
+                //result.Successful
+
+                currentAPSession.Items.ItemReceived += onReceiveItem;
+                //currentAPSession.Socket.PacketReceived += Socket_PacketReceived;
+
+                // Set default storage values
+                currentAPSession.DataStorage["StrawberryCount"].Initialize(0);
+            }
+        }
+
+        /*private void Socket_PacketReceived(ArchipelagoPacketBase packet)
+        {
+            if (packet.PacketType == ArchipelagoPacketType.ReceivedItems)
+            {
+                var receivedItemsPacket = (ReceivedItemsPacket)packet;
+
+                foreach(var item in receivedItemsPacket.Items)
+                {
+                    if (ItemMap.ContainsKey(item.Item))
+                    {
+                        string itemName = ItemMap[item.Item];
+                        Logger.Log("AP Item Received", itemName);
+
+                        switch (itemName)
+                        {
+                            case "Strawberry":
+                                currentAPSession.DataStorage["StrawberryCount"]++;
+
+                                Logger.Log("Strawberry Count", currentAPSession.DataStorage["StrawberryCount"]);
+                                break;
+                        }
+                    }
+                }
+            }
+            packet.ToString();
+        }*/
+
+        private void onReceiveItem(ReceivedItemsHelper receivedItemsHelper)
+        {
+            var itemReceivedName = receivedItemsHelper.PeekItemName();
+            Logger.Log("AP Item recieved", itemReceivedName);
+
+            if (itemReceivedName == "Strawberry")
+            {
+                currentAPSession.DataStorage["StrawberryCount"]++;
+
+                Logger.Log("Strawberry Count", currentAPSession.DataStorage["StrawberryCount"]);
+            }
+
+            receivedItemsHelper.DequeueItem();
+        }
+
+        private static Dictionary<long, string> ItemMap = new Dictionary<long, string>
+        {
+            { 77000, "Strawberry" }
+        };
     }
 }
